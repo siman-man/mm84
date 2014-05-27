392b20a7411976ac2e2726fb240956dd50302f7b
siman-man <k128585@ie.u-ryukyu.ac.jp>
2014-05-27 23:31:13 +0900

    temp save

--- RectanglesAndHoles.cpp
+++ RectanglesAndHoles.cpp
@@ -50,6 +50,18 @@ struct fieldInfo {
   }    
 };
 
+struct coord {
+  coord( int ypos = -1, int xpos = -1, int r = 0 ){
+    y = ypos;
+    x = xpos;
+    rot = r;
+  }
+
+  int y;
+  int x;
+  int rot;
+};
+
 struct rect_t {
   rect_t(short i = -1, short h = -1, short w = -1){
     id = i;
@@ -73,6 +85,7 @@ vector<int> XS;
 
 vector<rect_t> rect_list(MAX_N);
 vector< vector<int> > field;
+vector<int> ret;
 
 class RectanglesAndHoles {
   public:
@@ -82,19 +95,14 @@ class RectanglesAndHoles {
      */
     vector<int> place( vector<int> A, vector<int> B ){
       N = A.size();
-      vector<int> ret(3*N);
+      ret = vector<int>(3*N);
 
       init( A, B );
 
-      for(int i = 0; i < 3*N; i++){
-        ret[i*3] = xor128() % 1000000 * ( xor128()%2 )? -1 : 1;
-        ret[i*3+1] = xor128() % 1000000 * ( xor128()%2 )? -1 : 1;
-        ret[i*3+2] = 0;
-
-        LY[i] = ret[i*3];
-        LX[i] = ret[i*3];
-        RY[i] = LY[i] + ( ret[i*3+2] )? A[i] : B[i];
-        RX[i] = LX[i] + ( ret[i*3+2] )? B[i] : A[i];
+      for(int i = 1; i < N; i++){
+        int id = rect_list[i].id;
+        coord ans = solver( id );
+        setRect( id, ans.y, ans.x, ans.rot );
       }
  
       /*
@@ -110,6 +118,26 @@ class RectanglesAndHoles {
       return ret;
     };
 
+    int calcEval(){
+      int eval = 0;
+      return eval;
+    }
+
+    coord solver( int id ){
+      coord best_coord;
+
+      for(int i = 0; i < setCnt; i++){
+        vector<coord> coord_list = getCoordList(i);
+
+        for(int d = 0; d < 4; d++){
+          coord c = coord_list[d];
+          best_coord = c;
+        }
+      }
+
+      return best_coord;
+    }
+
     void init( vector<int> &A, vector<int> &B ){
       setCnt = 0;
 
@@ -123,6 +151,8 @@ class RectanglesAndHoles {
       }
 
       sort( rect_list.begin(), rect_list.begin()+N );
+
+      setRect( rect_list[0].id, 0, 0, 0 );
     }
 
     void gravity( int id, int rot ){
@@ -135,6 +165,17 @@ class RectanglesAndHoles {
 
       while( field[fromY][fromX] != EMPTY ){
       }
+    }
+
+    vector<coord> getCoordList( int id ){
+      vector<coord> coord_list(4);
+
+      coord_list[0] = coord( LY[id], LX[id] );
+      coord_list[1] = coord( LY[id], RX[id] );
+      coord_list[2] = coord( RY[id], LX[id] );
+      coord_list[3] = coord( RY[id], RX[id] );
+
+      return coord_list;
     }
 
     void createField(){
@@ -178,8 +219,8 @@ class RectanglesAndHoles {
 
       int qBeg = 0, qEnd = 0;
 
-      for(int y = 0; y < ysize-1; y++){
-        for(int x = 0; x < xsize-1; x++){
+      for(int y = 0; y < ysize; y++){
+        for(int x = 0; x < xsize; x++){
           if( field[y][x] == UNKNOWN ){
             QX[qBeg] = x;
             QY[qBeg++] = y;
@@ -240,10 +281,20 @@ class RectanglesAndHoles {
     }
 
     void setRect( int id, int y, int x, int rot ){
-      LY[id] = y;
-      LX[id] = x;
-      RY[id] = y + ( rot )? rect_list[id].height : rect_list[id].width;
-      RX[id] = y + ( rot )? rect_list[id].width : rect_list[id].height;
+      LY[setCnt] = y;
+      LX[setCnt] = x;
+      RY[setCnt] = y + (( rot )? rect_list[id].width : rect_list[id].height);
+      RX[setCnt] = x + (( rot )? rect_list[id].height : rect_list[id].width);
+      //fprintf( stderr, "id = %d, y = %d, x = %d, rot = %d\n", id, y, x, rot );
+      //fprintf( stderr, "width = %d, height = %d\n", rect_list[id].width, rect_list[id].height );
+
+      ret[id*3] = y;
+      ret[id*3+1] = x;
+      ret[id*3+2] = rot;
+
+      //fprintf( stderr, "Set LY = %d, LX = %d, RY = %d, RX = %d\n", LY[setCnt], LX[setCnt], RY[setCnt], RX[setCnt] );
+
+      setCnt++;
     }
 
     vector<int> enumerateCoordinates( int *A, int *B ){
@@ -252,7 +303,7 @@ class RectanglesAndHoles {
 
       set<int> coords;
 
-      for(int i = 0; i <= setCnt; i++){
+      for(int i = 0; i < setCnt; i++){
         max_value = max( max_value, max( A[i], B[i] ) );
         min_value = min( min_value, min( A[i], B[i] ) );
 
